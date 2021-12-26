b38678f56318d1127d1d4b6c4da8bc77a5c686e3
Khurram Iqbal <khurram.iqbal@veripark.com>
2021-12-26 19:26:06 +0500

    Filing image Data

--- MVVM-Practice/MVVM-Practice/Common/Utilities/Filing.swift
+++ MVVM-Practice/MVVM-Practice/Common/Utilities/Filing.swift
@@ -9,14 +9,68 @@ import Foundation
 
 final class Filing: NSObject{
     
-    static func saveFile(fileName: String, extension: String){
-        
+    static let sharedInstance = Filing.init()
+    private override init(){
+        super.init()
     }
-    static func getFile(fileName: String)->Data?{
+    func saveFile(data: Data, fileName: String, fileExtension: String){
+        
+         self.save(data: data, toDirectory: "images", withFileName: fileName + fileExtension)
+    }
+     func getFile(fileName: String)->Data?{
+        
+         let filePath = "images/" + fileName
+         return self.read(fromDocumentsWithFileName: filePath)
+      
+    }
+    private func read(fromDocumentsWithFileName fileName: String) -> Data? {
+       
+        guard let filePath = self.append(toPath: self.documentDirectory(), withPathComponent: fileName), let url = URL(string: filePath) else {return nil}
+        
+        do {
+            let data = try Data(contentsOf: url)
+            
+            return data
+        } catch {
+            print("Error reading saved file")
+            return nil
+           
+        }
+    }
+    
+   private  func save(data: Data ,
+                      toDirectory directory: String,
+                      withFileName fileName: String) {
+        guard let filePath = self.append(toPath: directory,
+                                         withPathComponent: fileName), let url = URL(string: filePath) else {
+            return
+        }
+        
+        do {
+            try data.write(to: url, options: .atomic)
+        } catch {
+            print("Error", error)
+            return
+        }
+        
+        print("Save successful")
+    }
+    private func append(toPath path: String,
+                        withPathComponent pathComponent: String) -> String? {
+        if var pathURL = URL(string: path) {
+            pathURL.appendPathComponent(pathComponent)
+            
+            return pathURL.absoluteString
+        }
         
         return nil
     }
-   
+    private func documentDirectory() -> String {
+        let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory,
+            .userDomainMask,true)
+        
+        return documentDirectory[0]
+    }
 }
 
 
